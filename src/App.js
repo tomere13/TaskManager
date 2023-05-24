6e6a742cd1e7ab6e1d30333191671150bf8828ed
tomere13 <98044440+tomere13@users.noreply.github.com>
2023-05-25 02:02:07 +0300

    first com5

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ export default function App() {
         )}
         <Routes>
           <Route
-            path="/"
+            path="/login"
             element={
               user ? (
                 <Navigate to="/dashboard" replace />
